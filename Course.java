c6dce9aa166952738d737ad876180d49992a17d2
Jdominguez991 <73549619+Jdominguez991@users.noreply.github.com>
2021-04-14 18:28:42 -0400

    Two new classes(students, courses)

---
+++ Course.java
@@ -0,0 +1,32 @@
+public class Course {
+    protected String name;
+    protected Student[] students;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public void listCourseStudents(){
+
+    }
+
+    public void addStudent(Student student){
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String name;
+    private Course[] courses;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Course[] courses) {
+        this.courses = courses;
+    }
+
+    public String toString(){
+
+    }
+
+    public void listStudentCourses(){
+
+    }
+    public void addCourse(Course course){
+
+    }
+}
